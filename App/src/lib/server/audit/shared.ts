49f0daeded973e1419d4da1148995ed2ed5b8044
Irakli <irakli_antidze@yahoo.com>
2026-05-29 11:33:26 +0400

    Add additional headers for HTTP requests

--- App/src/lib/server/audit/shared.ts
+++ App/src/lib/server/audit/shared.ts
@@ -214,6 +214,17 @@ export async function fetchText(url: string, options: FetchTextOptions = {}) {
 		responseType: 'text',
 		headers: {
 			'User-Agent': USER_AGENT,
+			'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+			'Accept-Language': 'en-US,en;q=0.9',
+			'Accept-Encoding': 'gzip, deflate, br',
+			'Upgrade-Insecure-Requests': '1',
+			'Sec-Ch-Ua': '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
+			'Sec-Ch-Ua-Mobile': '?0',
+			'Sec-Ch-Ua-Platform': '"Windows"',
+			'Sec-Fetch-Dest': 'document',
+			'Sec-Fetch-Mode': 'navigate',
+			'Sec-Fetch-Site': 'none',
+			'Sec-Fetch-User': '?1',
 			...options.headers
 		}
 	});
